790b329807e1818d6677cfe8a1d84a9d3066bf4c
MiguelF27 <miguel.cunha240512@gmail.com>
2026-05-06 11:00:44 -0300

    Projeto criado

---
+++ src/test/java/com/fullstack_curso/fullstack/FullstackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fullstack_curso.fullstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
